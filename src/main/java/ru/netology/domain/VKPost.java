e8fc6b4da7ca5161372c5240e51eb406f65b5fa2
Kirill <kiruxastar@gmail.com>
2020-09-28 12:09:07 +0300

    Initial commit v1

---
+++ src/main/java/ru/netology/domain/VKPost.java
@@ -0,0 +1,17 @@
+package ru.netology.domain;
+
+public class VKPost {
+    private String postName;
+    private String postingTime;
+    private String text;
+    private String imageURL;
+    private int likes;
+    private int quantityComments;
+    private int viewers;
+    private String comments;
+
+    public String generateBlock() {
+// TODO: add logic
+        return null;
+    }
+}
